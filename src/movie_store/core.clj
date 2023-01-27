ad068163b3e19a68b51ee425c24e304b7957e774
ACO <acopetrovic57@gmail.com>
2023-01-28 00:43:26 +0100

    Begin

--- src/movie_store/core.clj
+++ src/movie_store/core.clj
@@ -5,6 +5,7 @@
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.adapter.jetty :refer [run-jetty] :as jetty]
+            [ring.util.response :as response]
             ))
 
 (def movies [{:id 1 :title "The Shawshank Redemption" :price 10.99}
@@ -58,15 +59,22 @@
 (defn bootstrap-handler [request]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    (html [:head [:link {:rel "stylesheet" :href "{{webjars/path \"bootstrap/4.6.1/css/bootstrap.min.css\"}}"}]
-                   [:link {:rel "stylesheet" :href "~/css/site.css"}]
-                   [:link {:rel "stylesheet" :href "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"}]
-                   [:body [:nav [:label {:class "logo"} "eTickets"
-                                 [:ul [:li [:a {:href "#"}] "Home"]
-                                      [:li [:a {:href "#"}] "About"]]]]
-                    [:p "working!"]]])})
-
-
+   :body     (html [:head [:link {:rel "stylesheet" :href "{{webjars/path \"bootstrap/4.6.1/css/bootstrap.min.css\"}}"}]
+                    [:link {:rel "stylesheet" :href "~/css/site.css"}]
+                    [:link {:rel "stylesheet" :href "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"}]
+                    [:script {:src "https://kit.fontawesome.com/a076d05399.js"}]
+                    [:body {:style "background-color:2c3e50"}[:nav {:style "background-color:#0082e6; height:80px; width:100%"}
+                                                              [:input#check> {:type "checkbox"}]
+                                                              [:label {:for "check"}
+                                                               [:i.fas.fa-bars]]
+                                       [:label {:style "color:white; font-size:35px; padding: 0 100px; line-height: 80px" } "eTickets"
+                                        [:ul {:style "float:right; margin-right:20px; font-size:17px; padding:7px 13px; border-radius:3px; text-transform:uppercase"}
+                                          [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: 5s;padding:7px 13px;border-radius:3px"}"Home"]]
+                                          [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: 5s;padding:7px 13px;border-radius:3px"}"Movies"]]
+                                          [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: 5s;padding:7px 13px;border-radius:3px"}"Actors"]]
+                                          [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: 5s;padding:7px 13px;border-radius:3px"}"Producers"]]
+                                          [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: 5s;padding:7px 13px;border-radius:3px"}"Cinemas"]]]]]
+                     [:p "working!"]]])})
 
 
 
@@ -77,4 +85,4 @@
 
 
 
-(jetty/run-jetty (fn [req] (handler req)) {:port 4002 :join? false})
+(jetty/run-jetty (fn [req] (handler req)) {:port 4003 :join? false})
