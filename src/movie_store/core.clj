ee30820ef79a2c7526613c352b231453c15f9e1b
ACO <acopetrovic57@gmail.com>
2023-02-08 20:53:47 +0100

    Begin

--- src/movie_store/core.clj
+++ src/movie_store/core.clj
@@ -11,7 +11,10 @@
 (def movies [{:id 1 :title "The Shawshank Redemption" :price 10.99}
              {:id 2 :title "The Godfather" :price 12.99}
              {:id 3 :title "The Dark Knight" :price 14.99}])
-(movies)
+
+(def producers [])
+(def actors [])
+(defn cinemas [])
 (defn total-price [movies]
   (reduce #(+ %1 (get %2 :price 0)) 0 movies))
 (total-price movies)
@@ -63,70 +66,103 @@
                    [:link {:rel "stylesheet" :href "~/css/site.css"}]
                    [:link {:rel "stylesheet" :href "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"}]
                    [:script {:src "https://kit.fontawesome.com/a076d05399.js"}]
-                   [:body {:style "background-color:white"} [:nav {:style "background-color:#0082e6; height:80px; width:100%"}
+                   [:body {:style "background-color:white; container:fluid;min-height:100vh;line-height:1.5"} [:nav {:style "background-color:#0082e6; height:80px; width:100%"}
                                                              [:input#check> {:type "checkbox" :style "display:none; max-width: 952px;"}]
-                                                             [:label {:for "check" :style "font-size:30px; color:white; float:right; line-height:50px; margin-right:40px; cursor:pointer; display:none"}
+                                                             [:label {:for "check" :style "font-size:30px; color:white; float:right; margin-right:40px;padding:20px;display:none;max-width:952px"}
                                                               [:i.fas.fa-bars]]
                                                              [:label {:style "color:white; font-size:35px; padding: 0 100px; line-height: 50px"} "eTickets"
-                                                              [:ul {:style "float:right; margin-right:20px; font-size:17px; padding:7px 13px; border-radius:3px; text-transform:uppercase"}
-                                                               [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: .5s;padding:7px 13px;border-radius:3px; color:white;"} "Home"]]
-                                                               [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: .5s;padding:7px 13px;border-radius:3px; color:white;"} "Movies"]]
-                                                               [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: .5s;padding:7px 13px;border-radius:3px; color:white;"} "Actors"]]
-                                                               [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: .5s;padding:7px 13px;border-radius:3px; color:white;"} "Producers"]]
-                                                               [:li {:style "display:inline-block; line-height:50px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition: .5s;padding:7px 13px;border-radius:3px; color:white;"} "Cinemas"]]]]]
-                    [:div.container-fluid
-                     [:main.pb-3 {:role "main"}
-                      [:div.row
-                       [:div.col-md-4.col-xs-6.border-primary.mb-3
-                        [:div.card.mb-3 {:style "max-width: 240px; background-color:powderblue;"}
-                         [:div.row.g-0
-                          [:div.col-md-12
-                           [:div.card-header.text-white.bg-info
-                            [:p.card-text] [:h5.card-title "Cold Soles"
-                                            [:a.text-white.float-right {:href "/Movies/Edit/6"} [:i.bi.bi-pencil-square]]]
-                            [:p]]]
-                          [:div.col-md-6
-                           [:img {:src "http://dotnethow.net/images/movies/movie-8.jpeg" :width "100%" :alt "Cold Soles"}]]
-                          [:div.col-md-6
-                           [:div.card-body
-                            [:p.card-text "This is the Cold Soles movie description"]
-                            [:p.card-text [:b "Cinema:"] "Cinema 1"]
-                            [:p.card-text [:b "Category:"] "Drama"]
-                            [:p.card-text [:b "Start Date:"] "23 Jan 23"]
-                            [:p.card-text [:b "End Date:"] "09 Feb 23"]
-                            [:p.card-text [:b "Status:"]]]]
-                          [:div.col-md-12
-                           [:div.card-footer {:style "background-color:green"}
-                            [:p.card-text
-                             [:a.btn.btn-outline-primary.float-right {:href "/Movies/Details/6"} [:i.bi.bi-eye-fill " Show Details"]]
-                             [:a.btn.btn-success {:href "/Orders/AddItemToShoppingCart/6"} [:i.bi.bi-cart-plus] "Add to Cart"]]]]]]]
-                       [:div.col-md-4.col-xs-6.border-primary.mb-3
-                        [:div.card.mb-3 {:style "max-width: 240px;background-color:powderblue;"}
-                         [:div.row.g-0
-                          [:div.col-md-12
-                           [:div.card-header.text-white.bg-info
-                            [:p.card-text] [:h5.card-title "Ghost" [:a.text-white.float-right {:href "/Movies/Edit/3"} [:i.bi.bi-pencil-square]]]
-                            [:p]]]
-                          [:div.col-md-6
-                           [:img {:src "http://dotnethow.net/images/movies/movie-4.jpeg" :width "100%" :alt "Ghost"}]]
-                          [:div.col-md-6
-                           [:div.card-body
-                            [:p.card-text "This is the Ghost movie description"]
-                            [:p.card-text [:b "Cinema:"] "Cinema 3"]
-                            [:p.card-text [:b "Category:"] "Horror"]
-                            [:p.card-text [:b "Start Date:"] "10 Jan 23"]
-                            [:p.card-text [:b "End Date:"] "30 Feb 23"]
-                            [:p.card-text [:b "Status:"]]]]
-                          [:div.col-md-12
-                           [:div.card-footer {:style "background-color:green"}
-                            [:p.card-text
-                             [:a.btn.btn-outline-primary.float-right {:href "/Movies/Details/6"} [:i.bi.bi-eye-fill " Show Details"]]
-                             [:a.btn.btn-success {:href "/Orders/AddItemToShoppingCart/6"} [:i.bi.bi-cart-plus] "Add to Cart"]]]]]]]]]]]])})
+                                                              [:ul {:style "float:right; margin-right:20px; font-size:17px; padding:7px 13px; text-transform:uppercase"}
+                                                               [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "/movies" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} "Movies"]]
+                                                               [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} "Actors"]]
+                                                               [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} "Producers"]]
+                                                               [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} "Cinemas"]]]]]
+                    [:div
+                     [:div {:style "display:flex;align-items:center;width:100%"}
+                      [:div {:style "padding:20px"}
+                       [:div {:style "max-width:100%; background-color:powderblue;"}
+                        [:div
+                         [:div {:style "background-color:#0082e6"}
+                          [:div {:style "color:white;"}
+                           [:p] [:h2 {:style "padding-left:20px; line-height:80px;"} "Cold Soles"
+                                 [:a {:href "#":style "float:right"} [:i.bi.bi-pencil-square {:style "padding-right:20px;color:white "}]]]
+                           ]]
+                         [:div {:style "padding-left:10px;display:flex;"}
+                          [:img {:src "http://dotnethow.net/images/movies/movie-8.jpeg" :width "50%" :height "100%" :alt "Cold Soles"}]
+                          [:div
+                           [:div {:style "padding:10px; width:100%"}
+                            [:p "This is the Cold Soles movie description"]
+                            [:p [:b "Cinema:"] "Cinema 1"]
+                            [:p [:b "Category:"] "Drama"]
+                            [:p [:b "Start Date:"] "23 Jan 23"]
+                            [:p [:b "End Date:"] "09 Feb 23"]
+                            [:p [:b "Status:"]]]] ]
 
-[:div.col-md-6]
+                         [:div
+                          [:div {:style "background-color:#0082e6;"}
+                           [:p
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;"}[:a {:href "/Orders/AddItemToShoppingCart/6":style "background-color:#048A5B;padding:7px 20px;border-radius:20px;font-size:20px;color:white"} [:i.bi.bi-cart-plus] "Add to Cart"]]
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;float:right"}[:a {:href "/Movies/Details/6" :style "background-color:#1b9bff;padding:7px 20px;border-radius:20px;font-size:20px;color:white;"} [:i.bi.bi-eye-fill] "Show Details"]]]]]
+
+                         ]]]
+                      [:div {:style "padding:20px"}
+                       [:div {:style "max-width:100%;background-color:powderblue;"}
+                        [:div
+                         [:div {:style "background-color:#0082e6"}
+                          [:div {:style "color:white"}
+                           [:p] [:h2 {:style "padding-left:20px; line-height:80px;"} "Ghost"
+                                 [:a {:href "#" :style "float:right"} [:i.bi.bi-pencil-square {:style "padding-right:20px;color:white "}]]]]]
+                         [:div {:style "padding-left:10px;display:flex;"}
+                          [:img {:src "http://dotnethow.net/images/movies/movie-4.jpeg" :width "50%" :height "100%" :alt "Ghost"}]
+                          [:div
+                           [:div {:style "padding:10px; width:100%"}
+                            [:p "This is the Ghost movie description"]
+                            [:p [:b "Cinema:"] "Cinema 3"]
+                            [:p [:b "Category:"] "Horror"]
+                            [:p [:b "Start Date:"] "10 Jan 23"]
+                            [:p [:b "End Date:"] "28 Feb 23"]
+                            [:p [:b "Status:"]]]]]
+
+                         [:div
+                          [:div {:style "background-color:#0082e6;"}
+                           [:p
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;"}[:a {:href "/Orders/AddItemToShoppingCart/6":style "background-color:#048A5B;padding:7px 20px;border-radius:20px;font-size:20px;color:white"} [:i.bi.bi-cart-plus] "Add to Cart"]]
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;float:right"}[:a {:href "/Movies/Details/6" :style "background-color:#1b9bff;padding:7px 20px;border-radius:20px;font-size:20px;color:white;"} [:i.bi.bi-eye-fill] "Show Details"]]
+                            ]]]]]]
+
+                      [:div {:style "padding:20px"}
+                       [:div {:style "max-width:100%;background-color:powderblue;"}
+                        [:div
+                         [:div {:style "background-color:#0082e6"}
+                          [:div {:style "color:white"}
+                           [:p] [:h2 {:style "padding-left:20px; line-height:80px;"} "Scoob"
+                                 [:a {:href "#" :style "float:right"} [:i.bi.bi-pencil-square {:style "padding-right:20px;color:white "}]]]]]
+                         [:div {:style "padding-left:10px;display:flex;"}
+                          [:img {:src "http://dotnethow.net/images/movies/movie-7.jpeg" :width "50%" :height "100%" :alt "Ghost"}]
+                          [:div
+                           [:div {:style "padding:10px; width:100%"}
+                            [:p "This is the Scoob movie description"]
+                            [:p [:b "Cinema:"] "Cinema 2"]
+                            [:p [:b "Category:"] "Cartoon"]
+                            [:p [:b "Start Date:"] "08 Jan 23"]
+                            [:p [:b "End Date:"] "22 Feb 23"]
+                            [:p [:b "Status:"]]]]]
+
+                         [:div
+                          [:div {:style "background-color:#0082e6;"}
+                           [:p
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;"}[:a {:href "/Orders/AddItemToShoppingCart/6":style "background-color:#048A5B;padding:7px 20px;border-radius:20px;font-size:20px;color:white"} [:i.bi.bi-cart-plus] "Add to Cart"]]
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;float:right"}[:a {:href "/Movies/Details/6" :style "background-color:#1b9bff;padding:7px 20px;border-radius:20px;font-size:20px;color:white;"} [:i.bi.bi-eye-fill] "Show Details"]]
+                            ]]]]]]
+
+                      ]]]])})
+
+;display:flex;flex-direction:row;flex-wrap:wrap;
+;[:li {:style "display:inline-block; line-height:50px; margin:0 5px"}[:a {:href "/Movies/Details/6" :style "float:right;margin-right:20px;color:white;font-size:20px;background-color:#1b9bff;"} [:i.bi.bi-eye-fill] "Show Details"]]
+;                             [:li {:style "display:inline-block; line-height:50px; margin:0 5px"}[:a {:href "/Orders/AddItemToShoppingCart/6":style "display:inline-block;margin-left:20px;color:white;font-size:20px;background-color:#048A5B"} [:i.bi.bi-cart-plus] "Add to Cart"]]]]]]]]
+
 (defroutes handler
            (GET "/price" [] (total-price-view (total-price movies)))
-           (GET "/bootstrap" [] bootstrap-handler))
+           (GET "/movies" [] bootstrap-handler))
 
 
 
