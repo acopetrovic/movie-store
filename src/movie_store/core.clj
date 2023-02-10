8977edacce7d01a1ab163c4c7282d75ed4eac692
ACO <acopetrovic57@gmail.com>
2023-02-10 20:40:18 +0100

    Begin

--- src/movie_store/core.clj
+++ src/movie_store/core.clj
@@ -6,60 +6,31 @@
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.adapter.jetty :refer [run-jetty] :as jetty]
             [ring.util.response :as response]
+            [movie-store.actors :as actor]
+            [movie-store.producers :as producer]
+            [movie-store.cinemas :as cinema]
             ))
 
 (def movies [{:id 1 :title "The Shawshank Redemption" :price 10.99}
              {:id 2 :title "The Godfather" :price 12.99}
              {:id 3 :title "The Dark Knight" :price 14.99}])
+;(get (first movies) :title)
 
-(def producers [])
-(def actors [])
-(defn cinemas [])
+(defn total-name [movies]
+  (map :title movies))
+(total-name movies)
+;(doseq [movie movies]
+; (println (:title movie)))
+
 (defn total-price [movies]
   (reduce #(+ %1 (get %2 :price 0)) 0 movies))
 (total-price movies)
 
-(defn total-price-view [data]
-  (html (format "
-  <!DOCTYPE html>
-  <html>
-  <head>
-  <nav class=\"navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3\">
-    <title>Movie Store</title>
-    <nav class=\"navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3\">
-     </head>
-  <body>
-    <h1>Movie Title</h1>
-    <img src=\"https://st4.depositphotos.com/1533030/22618/v/1600/depositphotos_226181858-stock-illustration-cinema-poster-retro-film-projector.jpg\" width=\"300px\"alt=\"Movie Poster\">
-    <h2>Synopsis</h2>
-    <p>A brief summary of the movie's plot.</p>
-    <h2>Cast</h2>
-    <ul>
-      <li>Actor 1</li>
-      <li>Actor 2</li>
-      <li>Actor 3</li>
-    </ul>
-    <h2>Director</h2>
-    <p>Director's name</p>
-    <h2>Release Date</h2>
-    <h3 style=\"color:blue;\">A Blue Heading</h3>
-    <p>The movie's release date</p>
-    <button class=\"btn btn-primary\">Click me</button>
-    <h1>Total price of all movies is</h1>
-  </body>
-  </html>" data)))
-(total-price-view 30)
 
 
-;(defn handler [request]
-;(case (:uri request)
-; "/price"
-;{:status  200
-; :headers {"Content-type" "text/html"}
-; :body    (total-price-view 30)} ) )
-(total-price-view (total-price movies))
 
-(defn bootstrap-handler [request]
+
+(defn movie-handler [request]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    (html [:head [:link {:rel "stylesheet" :href "{{webjars/path \"bootstrap/4.6.1/css/bootstrap.min.css\"}}"}]
@@ -67,15 +38,15 @@
                    [:link {:rel "stylesheet" :href "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"}]
                    [:script {:src "https://kit.fontawesome.com/a076d05399.js"}]
                    [:body {:style "background-color:white; container:fluid;min-height:100vh;line-height:1.5"} [:nav {:style "background-color:#0082e6; height:80px; width:100%"}
-                                                             [:input#check> {:type "checkbox" :style "display:none; max-width: 952px;"}]
-                                                             [:label {:for "check" :style "font-size:30px; color:white; float:right; margin-right:40px;padding:20px;display:none;max-width:952px"}
-                                                              [:i.fas.fa-bars]]
-                                                             [:label {:style "color:white; font-size:35px; padding: 0 100px; line-height: 50px"} "eTickets"
-                                                              [:ul {:style "float:right; margin-right:20px; font-size:17px; padding:7px 13px; text-transform:uppercase"}
-                                                               [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "/movies" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} "Movies"]]
-                                                               [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} "Actors"]]
-                                                               [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} "Producers"]]
-                                                               [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "#" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} "Cinemas"]]]]]
+                                                                                                               [:input#check> {:type "checkbox" :style "display:none; max-width: 952px;"}]
+                                                                                                               [:label {:for "check" :style "font-size:30px; color:white; float:right; margin-right:40px;padding:20px;display:none;max-width:952px"}
+                                                                                                                [:i.fas.fa-bars]]
+                                                                                                               [:label {:style "color:white; font-size:35px; padding: 0 100px; line-height: 50px"} "eTickets"
+                                                                                                                [:ul {:style "float:right; margin-right:20px; font-size:17px; padding:7px 13px; text-transform:uppercase"}
+                                                                                                                 [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "/movies" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} [:i.bi.bi-film {:style "margin:0 3px"}] "Movies"]]
+                                                                                                                 [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "/actors" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} [:i.bi.bi-person-square {:style "margin:0 3px"}] "Actors"]]
+                                                                                                                 [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "/producers" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} [:i.bi.bi-headset {:style "margin:0 3px"}] "Producers"]]
+                                                                                                                 [:li {:style "display:inline-block; line-height:40px; margin:0 5px"} [:a {:href "/cinemas" :style "background-color:#1b9bff;transition:0.5s;padding:7px 13px;border-radius:3px; color:white;"} [:i.bi.bi-camera-reels {:style "margin:0 3px"}] "Cinemas"]]]]]
                     [:div
                      [:div {:style "display:flex;align-items:center;width:100%"}
                       [:div {:style "padding:20px"}
@@ -84,7 +55,7 @@
                          [:div {:style "background-color:#0082e6"}
                           [:div {:style "color:white;"}
                            [:p] [:h2 {:style "padding-left:20px; line-height:80px;"} "Cold Soles"
-                                 [:a {:href "#":style "float:right"} [:i.bi.bi-pencil-square {:style "padding-right:20px;color:white "}]]]
+                                 [:a {:href "#" :style "float:right"} [:i.bi.bi-pencil-square {:style "padding-right:20px;color:white "}]]]
                            ]]
                          [:div {:style "padding-left:10px;display:flex;"}
                           [:img {:src "http://dotnethow.net/images/movies/movie-8.jpeg" :width "50%" :height "100%" :alt "Cold Soles"}]
@@ -95,13 +66,13 @@
                             [:p [:b "Category:"] "Drama"]
                             [:p [:b "Start Date:"] "23 Jan 23"]
                             [:p [:b "End Date:"] "09 Feb 23"]
-                            [:p [:b "Status:"]]]] ]
+                            [:p [:b "Status:"]]]]]
 
                          [:div
                           [:div {:style "background-color:#0082e6;"}
                            [:p
-                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;"}[:a {:href "/Orders/AddItemToShoppingCart/6":style "background-color:#048A5B;padding:7px 20px;border-radius:20px;font-size:20px;color:white"} [:i.bi.bi-cart-plus] "Add to Cart"]]
-                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;float:right"}[:a {:href "/Movies/Details/6" :style "background-color:#1b9bff;padding:7px 20px;border-radius:20px;font-size:20px;color:white;"} [:i.bi.bi-eye-fill] "Show Details"]]]]]
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;"} [:a {:href "/Orders/AddItemToShoppingCart/6" :style "background-color:#048A5B;padding:7px 20px;border-radius:20px;font-size:20px;color:white"} [:i.bi.bi-cart-plus] "Add to Cart"]]
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;float:right"} [:a {:href "/Movies/Details/6" :style "background-color:#1b9bff;padding:7px 20px;border-radius:20px;font-size:20px;color:white;"} [:i.bi.bi-eye-fill] "Show Details"]]]]]
 
                          ]]]
                       [:div {:style "padding:20px"}
@@ -125,8 +96,8 @@
                          [:div
                           [:div {:style "background-color:#0082e6;"}
                            [:p
-                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;"}[:a {:href "/Orders/AddItemToShoppingCart/6":style "background-color:#048A5B;padding:7px 20px;border-radius:20px;font-size:20px;color:white"} [:i.bi.bi-cart-plus] "Add to Cart"]]
-                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;float:right"}[:a {:href "/Movies/Details/6" :style "background-color:#1b9bff;padding:7px 20px;border-radius:20px;font-size:20px;color:white;"} [:i.bi.bi-eye-fill] "Show Details"]]
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;"} [:a {:href "/Orders/AddItemToShoppingCart/6" :style "background-color:#048A5B;padding:7px 20px;border-radius:20px;font-size:20px;color:white"} [:i.bi.bi-cart-plus] "Add to Cart"]]
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;float:right"} [:a {:href "/Movies/Details/6" :style "background-color:#1b9bff;padding:7px 20px;border-radius:20px;font-size:20px;color:white;"} [:i.bi.bi-eye-fill] "Show Details"]]
                             ]]]]]]
 
                       [:div {:style "padding:20px"}
@@ -150,19 +121,18 @@
                          [:div
                           [:div {:style "background-color:#0082e6;"}
                            [:p
-                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;"}[:a {:href "/Orders/AddItemToShoppingCart/6":style "background-color:#048A5B;padding:7px 20px;border-radius:20px;font-size:20px;color:white"} [:i.bi.bi-cart-plus] "Add to Cart"]]
-                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;float:right"}[:a {:href "/Movies/Details/6" :style "background-color:#1b9bff;padding:7px 20px;border-radius:20px;font-size:20px;color:white;"} [:i.bi.bi-eye-fill] "Show Details"]]
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;"} [:a {:href "/Orders/AddItemToShoppingCart/6" :style "background-color:#048A5B;padding:7px 20px;border-radius:20px;font-size:20px;color:white"} [:i.bi.bi-cart-plus] "Add to Cart"]]
+                            [:p {:style "display:inline-block; line-height:70px; margin:0 5px;float:right"} [:a {:href "/Movies/Details/6" :style "background-color:#1b9bff;padding:7px 20px;border-radius:20px;font-size:20px;color:white;"} [:i.bi.bi-eye-fill] "Show Details"]]
                             ]]]]]]
 
                       ]]]])})
 
-;display:flex;flex-direction:row;flex-wrap:wrap;
-;[:li {:style "display:inline-block; line-height:50px; margin:0 5px"}[:a {:href "/Movies/Details/6" :style "float:right;margin-right:20px;color:white;font-size:20px;background-color:#1b9bff;"} [:i.bi.bi-eye-fill] "Show Details"]]
-;                             [:li {:style "display:inline-block; line-height:50px; margin:0 5px"}[:a {:href "/Orders/AddItemToShoppingCart/6":style "display:inline-block;margin-left:20px;color:white;font-size:20px;background-color:#048A5B"} [:i.bi.bi-cart-plus] "Add to Cart"]]]]]]]]
 
 (defroutes handler
-           (GET "/price" [] (total-price-view (total-price movies)))
-           (GET "/movies" [] bootstrap-handler))
+           (GET "/movies" [] movie-handler)
+           (GET "/actors" [] actor/actor-handler)
+           (GET "/producers" [] producer/producer-handler)
+           (GET "/cinemas" [] cinema/cinema-handler))
 
 
 
