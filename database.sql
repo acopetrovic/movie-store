d84986750259b2aa2fa4e31c01902221e0bbc428
ACO <acopetrovic57@gmail.com>
2023-02-16 19:45:34 +0100

    Begin

--- database.sql
+++ database.sql
@@ -61,3 +61,60 @@ values (1,'http://dotnethow.net/images/producers/producer-5.jpeg','Producer 5','
        (3,'http://dotnethow.net/images/producers/producer-3.jpeg','Producer 3','This is the Bio of the third producer'),
        (4,'http://dotnethow.net/images/producers/producer-2.jpeg','Producer 2','This is the Bio of the second producer'),
        (5,'http://dotnethow.net/images/producers/producer-1.jpeg','Producer 1','This is the Bio of the first producer');
+
+DROP TABLE IF EXISTS `movies`;
+
+CREATE TABLE `movies` (
+    `Id` int(11) NOT NULL AUTO_INCREMENT,
+    `Name` varchar(255) NULL,
+    `Description` varchar(255) NULL,
+    `Price` float(53) NOT NULL,
+    `ImageURL` varchar(255) NULL,
+    `StartDate` date NOT NULL,
+    `EndDate` date NOT NULL,
+    `MovieCategory` varchar(255) NULL,
+    `CinemaId` int NOT NULL,
+    `ProducerId`int NOT NULL,
+    PRIMARY KEY (`Id`),
+    CONSTRAINT `fk_id_cinemas` FOREIGN KEY (`CinemaId`) REFERENCES `cinemas` (`Id`) ON DELETE CASCADE,
+    CONSTRAINT `fk_id_producers` FOREIGN KEY (`ProducerId`) REFERENCES `producers` (`Id`) ON DELETE CASCADE
+);
+/*Data for table `movies`*/
+
+insert into `movies` (`Id`,`Name`,`Description`,`Price`,`ImageURL`,`StartDate`,`EndDate`,`MovieCategory`,`CinemaId`,`ProducerId`)
+values (1,'Scoob','This is the Scoob movie description','39.5','http://dotnethow.net/images/movies/movie-7.jpeg','27-Feb-23','29-Mar-23','Cartoon','1','3'),
+       (2,'Race','This is the Race movie description','39.5','http://dotnethow.net/images/movies/movie-6.jpeg','27-Feb-23','29-Mar-23','Cartoon','1','2'),
+       (3,'Ghost','This is the Ghost movie description','39.5','http://dotnethow.net/images/movies/movie-4.jpeg','27-Feb-23','29-Mar-23','Cartoon','4','4'),
+       (4,'The Shawshank Redemption','This is the The Shawshank Redemption movie description','29.5','http://dotnethow.net/images/movies/movie-1.jpeg','27-Feb-23','29-Mar-23','Cartoon','1','1'),
+       (5,'Life','This is the Life movie description','39.5','http://dotnethow.net/images/movies/movie-3.jpeg','27-Feb-23','29-Mar-23','Cartoon','3','3'),
+       (6,'Cold Soles','This is the Cold Soles movie description','39.5','http://dotnethow.net/images/movies/movie-8.jpeg','27-Feb-23','29-Mar-23','Cartoon','1','5');
+
+DROP TABLE IF EXISTS `actors_movies`;
+
+CREATE TABLE `actors_movies` (
+    `MovieId` int NOT NULL,
+    `ActorId` int NOT NULL,
+    PRIMARY KEY (`MovieId`,`ActorId`),
+    CONSTRAINT `fk_id_actors` FOREIGN KEY (`ActorId`) REFERENCES `actors` (`Id`) ON DELETE CASCADE,
+    CONSTRAINT `fk_id_movies` FOREIGN KEY (`MovieId`) REFERENCES `movies` (`Id`) ON DELETE CASCADE
+);
+/*Data for table `actors_movies`*/
+
+insert into `actors_movies` (`MovieId`,`ActorId`)
+values ('1','1'),
+       ('1','3'),
+       ('2','1'),
+       ('2','4'),
+       ('3','1'),
+       ('3','2'),
+       ('3','5'),
+       ('4','2'),
+       ('4','3'),
+       ('4','4'),
+       ('5','2'),
+       ('5','3'),
+       ('5','4'),
+       ('5','5'),
+       ('6','3'),
+       ('6','4'),
+       ('6','5');
