e09a6dee0a76ee833b23d44852542c1830bc4f70
ACO <acopetrovic57@gmail.com>
2023-03-05 14:11:05 +0100

    Begin

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -1,7 +1,7 @@
 function addActors(){
     $.confirm({
         title: 'Add Actor',
-        content: '<form action="/Domain/actors/{{actors.id}}/update" method="post">'+
+        content: '<form action="/Domain/actors/insert" method="post">'+
 '<div class="form-group text-center">'+
 '<label for="image text-center">Image URL</label>'+
 '<input type="text" class="form-control" name="profilepictureurl" style="width:40%;margin-left:auto;margin-right:auto;"/>' +
@@ -11,7 +11,7 @@ function addActors(){
 '<input type="text" class="form-control" name="fullname" style="width:40%;margin-left:auto;margin-right:auto;"/>' +
 '</div>'+
 '<div class="form-group text-center">'+
-'<label for="bio">Biography</label>'+
+'<label for="bio">Biography<br></label>'+
 '<input type="text" class="form-control" name="bio" style="width:40%;margin-left:auto;margin-right:auto;"/>'+
 '</div>'+
 '<button type="submit" class="btn btn-danger" style="float:right;margin-right:30%;">Save update</button>'+
